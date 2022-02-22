7915b80476b29f183fee489a4dfe8081d51b9529
Henry Ho <z5312521@ad.unsw.edu.au>
2022-02-22 20:00:43 +1100

    added scrapy spider

---
+++ PHASE_1/API_SourceCode/scraper/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import subprocess
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return {
+        'data' : 'Hello world!'
+    }
+
+@app.route('/update')
+def update():
+    spider_name = "donSpider"
+    # -O command overwrites the temporary file
+    # '&&' is used to join multiple commands together.
+    command = f"scrapy crawl {spider_name} -O latest.json && mv latest.json out.json"
+    s = subprocess.Popen(command.split(), shell=True)
+    return {
+        'data' : 0,
+    }
+
+@app.route('/articles', methods = ['GET'])
+def articles():
+    output_file = 'out.json'
+    with open(output_file) as output_data:
+        output = json.load(output_data)
+    return {
+        'data': output
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
