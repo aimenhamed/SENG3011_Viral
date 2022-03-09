5e8d1ab967de82a898133afb76c1efaee0981322
Henry Ho <z5312521@ad.unsw.edu.au>
2022-03-09 16:51:02 +1100

    updated sqitch to allow nulls in diseases and syndromes; location holds an array

--- PHASE_1/API_SourceCode/db/deploy/viral.sql
+++ PHASE_1/API_SourceCode/db/deploy/viral.sql
@@ -16,9 +16,9 @@ CREATE TABLE viral.report (
     report_id uuid NOT NULL DEFAULT gen_random_uuid(),
     article_id uuid NOT NULL,
     event_date text NOT NULL,
-    location text NOT NULL,
-    diseases text[] NOT NULL,
-    syndromes text[] NOT NULL,
+    location text[] NOT NULL,
+    diseases text[],
+    syndromes text[],
     CONSTRAINT fk_report_id_article FOREIGN KEY (article_id) references viral.article(article_id),
     CONSTRAINT pk_report_id PRIMARY KEY (report_id)
 );
