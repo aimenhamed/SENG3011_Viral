309620b43c5dd38904c1c6f78f4e695114e861eb
Leila Barry <z5206290@ad.unsw.edu.au>
2022-03-12 17:06:34 +1100

    report/dump done

---
+++ PHASE_1/API_SourceCode/src/api/services/Report.service.ts
@@ -0,0 +1,30 @@
+import { getLogger } from "../../utils/Logger";
+import { IReportsDumpSuccessResponse } from "IApiResponses";
+import { ReportRepository } from "../../repositories/Report.repository";
+import { ReportEntity } from "../../entity/Report.entity";
+import { HTTPError } from "../../utils/Errors";
+import { internalServerError } from "../../utils/Constants";
+
+export class ReportService {
+    private logger = getLogger();
+    constructor(readonly reportRepository : ReportRepository) {}
+
+    async getAllReports(): Promise<IReportsDumpSuccessResponse | undefined> {
+        const reports: ReportEntity[] =
+            await this.reportRepository.getAllReports();
+        
+        // if no reports in db then error
+        if (reports.length === 0) {
+            this.logger.error(`No reports found in db`);
+            throw new HTTPError(internalServerError);
+        }
+
+        // otherwise return reports
+        this.logger.info(`Reports found, responding to client`);
+        const result = {
+            reports,
+        };
+        return result;
+    }
+
+}
