bc891e81652b3e01aef426881f3dc258fca7d31d
Henry Ho <z5312521@ad.unsw.edu.au>
2022-03-22 17:57:26 +1100

    REFACTOR-002 rewritten sqitch to reflect database changes

--- PHASE_1/API_SourceCode/db/deploy/viral.sql
+++ PHASE_1/API_SourceCode/db/deploy/viral.sql
@@ -29,28 +29,50 @@ CREATE TABLE public.user (
     name text NOT NULL,
     email text NOT NULL,
     password text NOT NULL,
-    dashboards uuid[] NULL,
-    bookmarked_articles uuid[] NULL,
+    active_dashboard uuid NULL,
+    CONSTRAINT pk_active_dashboard FOREIGN KEY (active_dashboard) references public.dashboard(dashboard_id),
     CONSTRAINT pk_user_id PRIMARY KEY (user_id)
 );
 
 CREATE TABLE public.dashboard (
     dashboard_id uuid NOT NULL DEFAULT gen_random_uuid(),
-    user_id uuid NOT NULL,
-    widgets uuid[] NOT NULL,
-    CONSTRAINT fk_user_id_dashboard FOREIGN KEY (user_id) references public.user(user_id),
+    created_by uuid NOT NULL,
+    CONSTRAINT fk_user_id_dashboard FOREIGN KEY (created_by) references public.user(user_id),
     CONSTRAINT pk_dashboard_id PRIMARY KEY (dashboard_id)
 );
 
-CREATE TYPE widget AS ENUM ('ARTICLE');
+CREATE TYPE widgets AS ENUM ('ARTICLE', 'REPORT_COUNT', 'LATEST_ARTICLE', 'BOOKMARKED_ARTICLES', 'GRAPH', 'TEXT', 'MAP');
+CREATE TYPE graphs AS ENUM ('BAR', 'PIE', 'LINE', 'STACKED_BAR');
+CREATE TYPE axis AS ENUM ('TIME', 'TOTAL');
 CREATE TABLE public.widgets (
     widget_id uuid NOT NULL DEFAULT gen_random_uuid(),
     dashboard_id uuid NOT NULL,
-    widget_type widget NOT NULL,
-    article_id uuid NOT NULL,
+    widget_type widgets NOT NULL,
+    graph_type graphs NULL,
+    x_axis axis NULL,
+    y_axis axis NULL,
+    article_id uuid NULL,
+    key_terms text[] NULL,
+    content text NULL,
     CONSTRAINT fk_dashboard_id_widget FOREIGN KEY (dashboard_id) references public.dashboard(dashboard_id),
     CONSTRAINT fk_article_id_widget FOREIGN KEY (article_id) references public.article(article_id),
     CONSTRAINT pk_widget_id PRIMARY KEY (widget_id)
 );
+
+CREATE TABLE public.user_dashboards (
+    user_id uuid,
+    dashboard_id uuid,
+    CONSTRAINT pk_user_dashboards PRIMARY KEY (user_id, dashboard_id),
+    CONSTRAINT fk_user_id_user_dashboards FOREIGN KEY (user_id) references public.user(user_id),
+    CONSTRAINT fk_dashboard_id_user_dashboards FOREIGN KEY (dashboard_id) references public.dashboard(dashboard_id) 
+)
+
+CREATE TABLE public.user_bookmarked_articles (
+    user_id uuid,
+    article_id uuid,
+    CONSTRAINT pk_user_bookmarked_articles PRIMARY KEY (user_id, article_id),
+    CONSTRAINT fk_user_id_user_bookmarked_articles FOREIGN KEY (user_id) references public.user(user_id),
+    CONSTRAINT fk_article_id_user_bookmarked_articles FOREIGN KEY (article_id) references public.article(article_id)
+)
 
 COMMIT;
