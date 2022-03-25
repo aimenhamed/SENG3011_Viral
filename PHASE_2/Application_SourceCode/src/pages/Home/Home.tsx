3eb489b12a927ca5cdd5c94fd7122f8167845bdc
Shannen Jakosalem <z5164261@ad.unsw.edu.au>
2022-03-25 23:09:46 +1100

    set up world map using jvectormap on home page

--- PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
+++ PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
+import { VectorMap } from "@react-jvectormap/core";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "src/logic/redux/hooks";
 import { getSubscriptionDispatch, LoadingStatusTypes, selectAPP } from "src/logic/redux/reducers/subscriptionSlice/subscriptionSlice";
+import { worldMill } from '@react-jvectormap/world'
 import LoadingPage from "../../components/LoadingPage/LoadingPage";
 
 const Home = () => {
@@ -19,8 +21,29 @@ const Home = () => {
       return <div>Not onboarded</div>;
     }
 
+
+
+	/* const iProjectionObj: IProjection = {
+		type: string,
+		centralMeridian: number,
+	}
+
+	const iVectorMapObj: IVectorMap = {
+		name: "world_mill",
+    	content: {
+			insets: IInset[],
+			paths: PathsDefinition,
+			height: 100,
+			width: 100,
+			projection: iProjectionObj,
+		},
+	} */
+
     return (
-      <div>Hi</div>
+      <div style={{width: '100vw', height: '100vh'}}>
+		hi
+		<VectorMap map={worldMill} />
+      </div>
     );
   }
 
