3e5a12001c57edfcfd828e2d7dfbe5d553be7c42
Shannen Jakosalem <z5164261@ad.unsw.edu.au>
2022-03-26 17:27:04 +1100

    committing before making menubar

---
+++ PHASE_2/Application_SourceCode/src/components/MenuBar/MenuBar.tsx
@@ -0,0 +1,13 @@
+
+
+
+const MenuBar = () => {
+
+    return (
+        <div style={{backgroundColor: 'black'}}>
+            hello
+        </div>
+    )
+};
+
+export default MenuBar;
--- PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
+++ PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
@@ -6,6 +6,7 @@ import { worldMill } from '@react-jvectormap/world'
 import { ISVGElementStyleAttributes } from "@react-jvectormap/core/dist/types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import MenuBar from "src/components/MenuBar/MenuBar";
 import LoadingPage from "../../components/LoadingPage/LoadingPage";
 
 const Home = () => {
@@ -36,22 +37,22 @@ const Home = () => {
 		}
 
 		return (
-
-		<div style={{width: '100vw', height: '100vh', paddingRight: '50px', paddingLeft: '50px', boxSizing: 'border-box'}}>
-			<div style={{display: 'flex', flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between'}}>
-				<div>
-					<h1>
-						Select a destination
-					</h1>
+			<div style={{width: '100vw', height: '100vh', paddingRight: '50px', paddingLeft: '50px', boxSizing: 'border-box'}}>
+				<MenuBar />
+				<div style={{display: 'flex', flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between'}}>
+					<div>
+						<h1>
+							Select a destination
+						</h1>
+					</div>
+					<div>
+						<FontAwesomeIcon icon={faMagnifyingGlass} style={{position: 'relative', right: '-25px'}} />
+						<input type='text' style={{borderRadius: '5px', backgroundColor: '#e8e8e8', border: '0px', height: '25px', paddingLeft: '35px'}} placeholder='Search'></input></div>
+					</div>
+					<div style={{height: '80vh', paddingTop: '20px'}}>
+						<VectorMap map={worldMill} onRegionClick={(e, c) => regionClick(e, c)} backgroundColor='white' regionStyle={regionStyle} />
 				</div>
-				<div>
-					<FontAwesomeIcon icon={faMagnifyingGlass} style={{position: 'relative', right: '-25px'}} />
-					<input type='text' style={{borderRadius: '5px', backgroundColor: '#e8e8e8', border: '0px', height: '25px', paddingLeft: '35px'}} placeholder='Search'></input></div>
-				</div>
-				<div style={{height: '80vh', paddingTop: '20px'}}>
-					<VectorMap map={worldMill} onRegionClick={(e, c) => regionClick(e, c)} backgroundColor='white' regionStyle={regionStyle} />
-				</div>
-		</div>
+			</div>
 		);
 	}
 
