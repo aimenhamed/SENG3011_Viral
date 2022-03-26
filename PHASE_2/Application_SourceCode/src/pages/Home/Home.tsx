d44402dd32c633e53eadd0bfa6e65d00adc3cdb2
Shannen Jakosalem <z5164261@ad.unsw.edu.au>
2022-03-26 17:19:32 +1100

    added search bar, header, adjusted size of map and added padding

--- PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
+++ PHASE_2/Application_SourceCode/src/pages/Home/Home.tsx
@@ -3,51 +3,59 @@ import { useDispatch } from "react-redux";
 import { useAppSelector } from "src/logic/redux/hooks";
 import { getSubscriptionDispatch, LoadingStatusTypes, selectAPP } from "src/logic/redux/reducers/subscriptionSlice/subscriptionSlice";
 import { worldMill } from '@react-jvectormap/world'
+import { ISVGElementStyleAttributes } from "@react-jvectormap/core/dist/types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import LoadingPage from "../../components/LoadingPage/LoadingPage";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const {loadingStatus, app, error} = useAppSelector(selectAPP);
-  if (error) {
-    return <div>Error</div>;
-  }
+	const dispatch = useDispatch();
+	const {loadingStatus, app, error} = useAppSelector(selectAPP);
 
-  if (loadingStatus === LoadingStatusTypes.IDLE) {
-    dispatch(getSubscriptionDispatch());
-  }
-
-  if (loadingStatus === LoadingStatusTypes.GET_SUBSCRIPTION_COMPLETED) {
-    if (!app.subscriptionId) {
-      return <div>Not onboarded</div>;
-    }
-
-
-
-	/* const iProjectionObj: IProjection = {
-		type: string,
-		centralMeridian: number,
+	const regionStyle: ISVGElementStyleAttributes = {
+		initial: {
+			fill: 'green',
+		},
 	}
 
-	const iVectorMapObj: IVectorMap = {
-		name: "world_mill",
-    	content: {
-			insets: IInset[],
-			paths: PathsDefinition,
-			height: 100,
-			width: 100,
-			projection: iProjectionObj,
-		},
-	} */
+	const regionClick =  (e: Event, c: String) => {
+		console.log(c)
+	}
 
-    return (
-      <div style={{width: '100vw', height: '100vh'}}>
-		hi
-		<VectorMap map={worldMill} />
-      </div>
-    );
-  }
+	if (error) {
+		return <div>Error</div>;
+	}
 
-  return <LoadingPage />;
+	if (loadingStatus === LoadingStatusTypes.IDLE) {
+		dispatch(getSubscriptionDispatch());
+	}
+
+	if (loadingStatus === LoadingStatusTypes.GET_SUBSCRIPTION_COMPLETED) {
+		if (!app.subscriptionId) {
+		return <div>Not onboarded</div>;
+		}
+
+		return (
+
+		<div style={{width: '100vw', height: '100vh', paddingRight: '50px', paddingLeft: '50px', boxSizing: 'border-box'}}>
+			<div style={{display: 'flex', flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between'}}>
+				<div>
+					<h1>
+						Select a destination
+					</h1>
+				</div>
+				<div>
+					<FontAwesomeIcon icon={faMagnifyingGlass} style={{position: 'relative', right: '-25px'}} />
+					<input type='text' style={{borderRadius: '5px', backgroundColor: '#e8e8e8', border: '0px', height: '25px', paddingLeft: '35px'}} placeholder='Search'></input></div>
+				</div>
+				<div style={{height: '80vh', paddingTop: '20px'}}>
+					<VectorMap map={worldMill} onRegionClick={(e, c) => regionClick(e, c)} backgroundColor='white' regionStyle={regionStyle} />
+				</div>
+		</div>
+		);
+	}
+
+	return <LoadingPage />;
 };
 
 export default Home;
