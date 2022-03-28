5a65f6d27d0f8b236eed6239b8ae49b724e24c4e
Henry Ho <z5312521@ad.unsw.edu.au>
2022-03-28 13:34:28 +1100

    REFACTOR-005 added route this time

--- PHASE_1/API_SourceCode/src/modules/Database.ts
+++ PHASE_1/API_SourceCode/src/modules/Database.ts
@@ -9,7 +9,7 @@ import { UserEntity } from "../entity/User.entity";
 export default class Database {
   private logger = getLogger();
   constructor(readonly connectionName: string) {}
-  // testing new db: POSTGRESQL_USER=ytodeyyosrgpen POSTGRESQL_PASSWORD=8356074ac9f65aad0aa219bcf46118e9ea940529b2db3307ed143cc033034a38 POSTGRESQL_HOST=ec2-35-153-35-94.compute-1.amazonaws.com POSTGRESQL_DATABASE=db9penvi7i6jkk
+  // testing new db: POSTGRESQL_USER=fsggrwwrbsxbkk POSTGRESQL_PASSWORD=0bbabdc7704f4a9cc4b6fbf80a2acc5c8f5f0c70e6a9bcc5238a0e31b5191f56 POSTGRESQL_HOST=ec2-18-210-191-5.compute-1.amazonaws.com POSTGRESQL_DATABASE=dd4u9qmc5pk0gv
   // testing locally: POSTGRESQL_USER=postgres POSTGRESQL_PASSWORD=mysecretpassword POSTGRESQL_HOST=0.0.0.0 POSTGRESQL_DATABASE=mydb
 
   async start(): Promise<void> {
