b70fdd319ecafd206673edeb5231a166d5c7dc33
Henry Ho <z5312521@ad.unsw.edu.au>
2022-03-28 14:52:15 +1100

    IMPL-001 created route GET  /advice

---
+++ PHASE_1/API_SourceCode/src/api/services/Advice.service.ts
@@ -0,0 +1,33 @@
+import { getLogger } from "../../utils/Logger";
+import {
+    IAdviceSpecificSuccessResponse
+} from "IApiResponses";
+import {AdviceRepository} from "../../repositories/Advice.repository";
+import { HTTPError } from "../../utils/Errors";
+import { notFoundError } from "../../utils/Constants";
+import { convertAdviceEntityToInterface } from "../../converters/Advice.converter";
+import { getLog } from "../../utils/Helpers";
+
+export class AdviceService {
+    private logger = getLogger();
+    constructor(readonly adviceRepository: AdviceRepository) {}
+
+    async getAdvice(country: string): Promise<IAdviceSpecificSuccessResponse> {
+        const advice = await this.adviceRepository.getAdviceByCountry(country);
+
+        if (advice === undefined) {
+            this.logger.error(`No advice for country ${country} found in db`);
+            throw new HTTPError(notFoundError);
+        }
+
+           this.logger.info(
+            `Advice found with country ${country}, responding to client`
+        );
+        return {
+            advice: convertAdviceEntityToInterface(advice),
+            log: getLog(new Date()),
+        };
+    }
+        
+
+}
