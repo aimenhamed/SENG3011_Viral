6de8742d4969655e0f1242ae77e2a8910fff837b
Leila Barry <z5206290@ad.unsw.edu.au>
2022-03-29 16:38:56 +1100

    IMPL amadeus flight search api

--- PHASE_1/API_SourceCode/src/interfaces/IFetchResponses.ts
+++ PHASE_1/API_SourceCode/src/interfaces/IFetchResponses.ts
@@ -57,3 +57,90 @@ export interface AmadeusData {
     }
   ];
 }
+
+export interface AmadeusFlightResponse {
+  data: AmadeusFlightData[];
+}
+
+export interface AmadeusFlightData {
+      type: string,
+      id: string,
+      source: string,
+      instantTicketingRequired: boolean,
+      nonHomogenous: boolean,
+      oneWay: boolean,
+      lastTicketingDate: Date,
+      numberOfBookableSeats: number,
+      itineraries: [
+        {
+          duration: string,
+          segments: [
+            {
+              departure: {
+                iataCode: string,
+                termial: string,
+                at: string
+              },
+              arrival: {
+                iataCode: string,
+                terminal: string,
+                at: string
+              },
+              carrierCode: string,
+              number: string,
+              aircraft: {
+                code: string,
+              },
+              operating: {
+                carrierCode: string,
+              },
+              duration: string,
+              id: string,
+              numberOfStops: number,
+              blacklistedInEU: boolean
+            }
+          ]
+        }
+      ],
+      price: {
+        currency: string,
+        total: string,
+        base: string,
+        fees: [
+          {
+            amount: string,
+            type: string
+          },
+        ]
+        grandTotal: string,
+      },
+      pricingOptions: {
+        fareType: [string],
+        includedCheckedBagsOnly: boolean
+      },
+      validatingAirlineCodes: [string ],
+      travelerPricings: [
+        {
+          travelerId: string,
+          fareOption: string,
+          travelerType: string,
+          price: {
+            currency: string,
+            total: string,
+            base: string
+          },
+          fareDetailsBySegment: [
+            {
+              segmentId: string,
+              cabin: string,
+              fareBasis: string,
+              class: string,
+              includedCheckedBags: {
+                weight: number,
+                weightUnit: string
+              }
+            }
+          ]
+        }
+      ]
+}
