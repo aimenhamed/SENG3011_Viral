773a91a59f4e703893e64f72b0862f8af857061c
Aimen Hamed <aimenhamed@live.com.au>
2022-04-03 21:25:49 +1000

    IMPL-003 unit tests passing

--- PHASE_2/Application_SourceCode/src/helpers/rtl.tsx
+++ PHASE_2/Application_SourceCode/src/helpers/rtl.tsx
@@ -3,18 +3,36 @@ import { FC } from "react";
 import { Provider } from "react-redux";
 import { DeepPartial, ReducersMapObject } from "redux";
 import {
-  initialState,
+  initialState as articleState,
   articleSlice,
 } from "src/logic/redux/reducers/articleSlice/articleSlice";
+import {
+  initialState as adviceState,
+  adviceSlice,
+} from "src/logic/redux/reducers/adviceSlice/adviceSlice";
+import {
+  initialState as flightsState,
+  flightsSlice,
+} from "src/logic/redux/reducers/flightsSlice/flightsSlice";
 import { render as rtlRender } from "@testing-library/react";
+import {
+  initialState as userState,
+  userSlice,
+} from "src/logic/redux/reducers/userSlice.ts/userSlice";
 
 const preloadedInitialState = {
-  articles: initialState,
+  articles: articleState,
+  advice: adviceState,
+  flights: flightsState,
+  user: userState,
 };
 
 const getMockStore = (
   reducers: ReducersMapObject = {
+    advice: adviceSlice.reducer,
     articles: articleSlice.reducer,
+    flights: flightsSlice.reducer,
+    user: userSlice.reducer,
   },
   preloadedState: DeepPartial<any | undefined> = { preloadedInitialState }
 ) =>
@@ -29,7 +47,10 @@ const render = (
   {
     store = configureStore({
       reducer: {
+        advice: adviceSlice.reducer,
         articles: articleSlice.reducer,
+        flights: flightsSlice.reducer,
+        user: userSlice.reducer,
       },
       preloadedState: state || preloadedInitialState,
     }),
