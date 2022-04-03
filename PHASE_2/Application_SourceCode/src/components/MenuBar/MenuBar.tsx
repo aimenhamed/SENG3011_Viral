123277530301fc9aa1caa33a2beaac0d68ae7a50
Shannen Jakosalem <z5164261@ad.unsw.edu.au>
2022-04-03 13:56:51 +1000

    fixed styling of menubar. just have to fix icon

--- PHASE_2/Application_SourceCode/src/components/MenuBar/MenuBar.tsx
+++ PHASE_2/Application_SourceCode/src/components/MenuBar/MenuBar.tsx
@@ -15,25 +15,41 @@ const MenuBar = () => {
 	];
 
 	const highlightOnHover = (id: string) => {
-		const menuBtn = document.getElementById(id);
-		if (menuBtn != null) {
-			menuBtn.style.backgroundColor = '#2a9763';
-			menuBtn.style.borderRadius = '10px 0px 0px 10px';
+		const btn = document.getElementById(id);
+
+		if (btn != null && id !== 'signOutBtn') {
+			btn.style.backgroundColor = '#2a9763';
+			btn.style.borderRadius = '10px 0px 0px 10px';
+		} else if (btn != null && id === 'signOutBtn') {
+			btn.style.filter = 'brightness(1.1)';
 		}
 	}
 
 	const unHighlight = (id: string) => {
-		const menuBtn = document.getElementById(id);
-		if (menuBtn != null) {
-			menuBtn.style.backgroundColor = '#ffffff';
+		const btn = document.getElementById(id);
+		
+		if (btn != null && id !== 'signOutBtn') {
+			btn.style.backgroundColor = '#ffffff';
+		} else if (btn != null && id === 'signOutBtn') {
+			btn.style.filter = 'brightness(1.0)'
 		}
 	} 
 
     return (
         <div style={{width: '150px', boxShadow: '-10px 0px 25px', paddingLeft: '20px', boxSizing: 'border-box'}}>
-        	<div>
-               
+        	<div style={{height: '40px'}}></div>
+			<div style={{display: 'flex', flexDirection: 'row'}}>
+              <div style={{backgroundColor: '#b6b6b6', borderRadius: '50%', width: '50px', height: '50px', outline: 'auto', outlineColor: '#b6b6b6'}}>
+				<div style={{margin: 'auto'}}>
+					<FontAwesomeIcon icon={AllIcons.faUser} style={{color: 'white', marginLeft: '8px', marginTop: '10px', fontSize: '40px'}} />
+			  	</div>
+			  </div>
+			  <div style={{width: '10px'}}></div>
+			  <div>
+				  <p>Person</p>
+			  </div>
            	</div>
+			<div style={{height: '40px'}}></div>
 			<div>
 				{
 					menuItems.map((menuItem) => 
@@ -46,6 +62,15 @@ const MenuBar = () => {
 				}
 			</div>
 			<div style={{height: '100px'}}></div>
+			<div style={{marginLeft: '10px'}}>
+				<button id='signOutBtn' type='button' style={{paddingLeft: '10px', paddingRight: '10px', border: 'none', borderRadius: '10px', backgroundColor: '#d1d1d1', cursor: 'pointer'}} onMouseOver={() => highlightOnHover('signOutBtn')} onMouseLeave={() => unHighlight('signOutBtn')} onFocus={() => highlightOnHover('signOutBtn')}>
+					<div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+						<FontAwesomeIcon icon={AllIcons.faSignOut} />
+						<div style={{width: '10px'}}></div>
+						<p>Logout</p>
+					</div>
+				</button>
+			</div>
 			<div style={{position: 'absolute', bottom: '10px'}}>
 				<Icon />
 			</div>
